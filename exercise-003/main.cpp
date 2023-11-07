01d9ac63d396c82777e6c9cfe128f6d1e72ee325
lukas1-12 <122440412+lukas1-12@users.noreply.github.com>
2023-11-07 09:52:22 +0100

    Exercise 003
    Not sure if the capacity does it jobs, but looks good

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -14,6 +14,23 @@ auto main(int argc, char **argv) -> int
 
     /* INSERT YOUR CODE HERE */
     VectorInt data(20);
-
+    for(int i=0; i < data.max_size(); i++) {
+        data.push_back(i*2);
+    }
+    data.push_back(42);
+    data.push_back(48);
+    for(int i=0; i < data.size(); i++) {
+        fmt::print("data[{}] = {}\n", i, data.at(i));
+    }
+    fmt::print("data.size() = {}\n", data.size());
+    //data.resize(42);
+    //fmt::print("data.size() = {}\n", data.size());
+    fmt::print("Pushing back 77\n");
+     auto start = std::chrono::system_clock::now();
+    data.push_back(77);
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    fmt::print("data[{}] = {}\n", data.size()-1, data.at(data.size()-1));
+    fmt::print("The push back took {}\n", elapsed);
     return 0; /* exit gracefully*/
 }
--- exercise-003/vectorint.hpp
+++ exercise-003/vectorint.hpp
@@ -5,8 +5,16 @@ class VectorInt {
 public:
     VectorInt(int size);
     ~VectorInt();
+    void push_back(int data);
+    int size();
+    int max_size();
+    void resize(int new_size);
+    void clear(int pos);
+    int at(int pos);
+
 private:
     int m_size;
+    int capacity = 100;
     int* mp_Data;
 };
 
