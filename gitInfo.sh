b5d479eaa81db92facc48a1e2020f610c3cc1a59
lukas1-12 <122440412+lukas1-12@users.noreply.github.com>
2023-11-07 10:18:52 +0100

    gitInfo.sh - extended

--- gitInfo.sh
+++ gitInfo.sh
@@ -4,4 +4,10 @@ git merge --all --> merge all branches from origin (main from Student)
 
 git checkout -b solution-003 upstream/exercise-003  --> create local branch from upstream (main from Teacher)
 
-git push -u origin solution-003 --> push local branch to origin (main from Student)
+git push -u origin solution-003 --> push local branch to origin (main from Student) as a branch
+
+git checkout main --> switch to main branch
+
+#on main branch
+git merge solution-003 --> merge solution-003 branch into main branch
+#here you have to commit everything / solve conflicts so that the push will work
